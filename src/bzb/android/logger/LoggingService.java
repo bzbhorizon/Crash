974b25e940eb60ec4b02cdd00f1054700f36faeb
bzbhorizon <bzb@cs.nott.ac.uk>
2010-04-01 08:23:31 +0100

    Added logging
    TODO: add timestamp to log
    TODO: work out scheduled logging (activity allows scheduling?)
    TODO: stop service sleeping when device sleeps
    TODO: make sure service stops logging before limit on sd card is reached
    TODO: test log size
    TODO: test log size with video

--- src/bzb/android/logger/LoggingService.java
+++ src/bzb/android/logger/LoggingService.java
@@ -3,13 +3,19 @@
  */
 package bzb.android.logger;
 
-import java.util.ArrayList;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
 
 import android.app.Service;
 import android.content.Intent;
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.Environment;
 import android.os.IBinder;
 import android.util.Log;
 
@@ -17,7 +23,9 @@ import android.util.Log;
  * @author bzb
  *
  */
-public class LoggingService extends Service {
+public class LoggingService extends Service implements SensorEventListener {
+
+	private PrintWriter captureFile;
 
 	public LoggingService () {
 		
@@ -36,14 +44,69 @@ public class LoggingService extends Service {
 		super.onCreate();
 		Log.i(getClass().getName(),"Started service");
 		
-		SensorManager sensorManager = 
-            (SensorManager)getSystemService( SENSOR_SERVICE  );
-		List<Sensor> sensors = sensorManager.getSensorList( Sensor.TYPE_ALL );
+		startLogging();
 	}
 
 	public void onDestroy() {
 		super.onDestroy();
+		
+		if (captureFile != null) {
+			captureFile.close();
+			Log.i(getClass().getName(),"Closed file");
+		}
+		
 		Log.i(getClass().getName(),"Destroyed service");
+	}
+	
+	private void startLogging () {
+		File captureFileName = new File( Environment.getExternalStorageDirectory(), "log.csv" );
+		Log.i(getClass().getName(),"Linked to file");
+		
+        try {
+            captureFile = new PrintWriter( new FileWriter( captureFileName, true ) );
+            Log.i(getClass().getName(),"Opened file " + captureFileName.getName());
+            
+            SensorManager sensorManager = 
+                (SensorManager)getSystemService( SENSOR_SERVICE  );
+    		List<Sensor> sensors = sensorManager.getSensorList( Sensor.TYPE_ALL );
+    		Log.i(getClass().getName(),"Listed sensors");
+    		
+    		int i = 0;
+    		for (Sensor sensor : sensors) {
+    			sensorManager.registerListener( 
+                        this, 
+                        sensor,
+                        SensorManager.SENSOR_DELAY_UI );
+    			Log.i(getClass().getName(),"Registered listener for sensor " + sensor.getName());
+    			captureFile.println(sensor.getType() + "," + sensor.getName() + "," + sensor.getResolution() + "," + sensor.getMaximumRange() + "," + sensor.getPower());
+    		}
+    		
+        } catch( IOException ex ) {
+            Log.e( getClass().getName(), ex.getMessage(), ex );
+        }
+	}
+
+	@Override
+	public void onAccuracyChanged(Sensor sensor, int accuracy) {}
+
+	@Override
+	public void onSensorChanged(SensorEvent event) {
+		/*StringBuilder b = new StringBuilder();
+        for( int i = 0 ; i < event.values.length ; ++i ) {
+            if( i > 0 )
+                b.append( " , " );
+            b.append( Float.toString( event.values[i] ) );
+        }*/
+        captureFile.print(event.sensor.getType() + ",");
+        //Log.i( getClass().getName(), event.sensor.getName() + " onSensorChanged" );
+        if( captureFile != null ) {
+                for( int i = 0 ; i < event.values.length ; ++i ) {
+                    if( i > 0 )
+                        captureFile.print( "," );
+                    captureFile.print( Float.toString( event.values[i] ) );
+                }
+                captureFile.println();
+        }
 	}
 
 }
